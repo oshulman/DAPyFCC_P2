70f9bd3b5585164792753ca22e564e34a899cc96
oshulman <oshulman@umich.edu>
2020-12-25 01:57:51 -0500

    calculate_demographic_data half written

--- boilerplate-demographic-data-analyzer-1/demographic_data_analyzer.py
+++ boilerplate-demographic-data-analyzer-1/demographic_data_analyzer.py
@@ -3,19 +3,35 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
-
+    df = pd.read_csv('adult.data.csv')
+  
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+  
+    # each of the races in the dataset
+    races = df['race'].unique()
+    # count of each race
+    counts = []
+    for race in races:
+      counts.append(df.loc[df['race'] == race].shape[0])
 
+    race_count = pd.Series(counts, index = races)
+    
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df.loc[df['sex'] == 'Male', 'age'].mean()
 
+    
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
-
+    percentage_bachelors = (df.loc[df['education'] == 'Bachelors'].shape[0] / df['education'].count()) * 100
+    
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
+
+    adv_degrees_over_50K = df.loc[((df['education'] == 'Bachelors') | (df['education'] == 'Masters') | (df['education'] == 'Doctorate')) & (df['salary'] == '>50K'), ['salary']].shape[0]
+    total_adv_degrees = df.loc[(df['education'] == 'Bachelors') | (df['education'] == 'Masters') | (df['education'] == 'Doctorate'), ['salary']].shape[0]
+    
+    per_adv_more_50K = (adv_degrees_over_50K / total_adv_degrees) * 100
+    return per_adv_more_50K
+
+    """
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
     higher_education = None
@@ -67,3 +83,4 @@ def calculate_demographic_data(print_data=True):
         highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
+    """
